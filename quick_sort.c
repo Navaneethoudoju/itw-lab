1c8d5ce8d2c4aaecea3a35f01503b20dcd19e728
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:19:52 +0530

    partition added

--- quick_sort.c
+++ quick_sort.c
@@ -13,3 +13,18 @@ void swap(struct ListNode *a, struct ListNode *b) {
     b->val = temp;
 }
 
+struct ListNode* partition(struct ListNode *low, struct ListNode *high) {
+    int pivot = high->val;
+    struct ListNode *i = low->prev;
+    for (struct ListNode *j = low; j != high; j = j->next) {
+        if (j->val <= pivot) {
+            i = (i == NULL) ? low : i->next;
+            swap(i, j);
+        }
+    }
+    i = (i == NULL) ? low : i->next;
+    swap(i, high);
+    return i;
+}
+
+
