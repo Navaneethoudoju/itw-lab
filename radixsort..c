70a52a566052445dc8a970c8cb9a26a4fcafceab
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:23:27 +0530

    getmax and count func added

---
+++ radixsort..c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+void countingSort(int arr[], int n, int exp) {
+    int* output = (int*)malloc(n * sizeof(int));  
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+
+    free(output);  
+}
+
+
