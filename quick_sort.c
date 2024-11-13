725c9bec7c7c2486685b60c12d254c0b3e299b4a
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:20:07 +0530

    partition added

--- quick_sort.c
+++ quick_sort.c
@@ -27,4 +27,48 @@ struct ListNode* partition(struct ListNode *low, struct ListNode *high) {
     return i;
 }
 
+void quickSort(struct ListNode *low, struct ListNode *high) {
+    if (high != NULL && low != high && low != high->next) {
+        struct ListNode *p = partition(low, high);
+        quickSort(low, p->prev);
+        quickSort(p->next, high);
+    }
+}
 
+struct ListNode *getLastNode(struct ListNode *head) {
+    while (head != NULL && head->next != NULL)
+        head = head->next;
+    return head;
+}
+
+void printList(struct ListNode *node) {
+    while (node != NULL) {
+        printf("%d ", node->val);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+struct ListNode* newNode(int data) {
+    struct ListNode* temp = (struct ListNode*)malloc(sizeof(struct ListNode));
+    temp->val = data;
+    temp->next = NULL;
+    return temp;
+}
+
+int main() {
+    struct ListNode *a = newNode(10);
+    a->next = newNode(7);
+    a->next->next = newNode(8);
+    a->next->next->next = newNode(9);
+    a->next->next->next->next = newNode(1);
+    a->next->next->next->next->next = newNode(5);
+
+    struct ListNode *last = getLastNode(a);
+    quickSort(a, last);
+
+    printf("Sorted Linked List is: \n");
+    printList(a);
+
+    return 0;
+}
