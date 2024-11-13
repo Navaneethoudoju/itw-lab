6f205c8ba130404cbb5687d874acd3e72cd14fb5
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:24:25 +0530

    radix sort added

--- radixsort..c
+++ radixsort..c
@@ -32,3 +32,128 @@ void countingSort(int arr[], int n, int exp) {
 }
 
 
+void radixSortArray(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
+}
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int getMaxLinkedList(struct Node* head) {
+    int max = head->data;
+    struct Node* current = head->next;
+    while (current != NULL) {
+        if (current->data > max)
+            max = current->data;
+        current = current->next;
+    }
+    return max;
+}
+
+void countingSortLinkedList(struct Node** headRef, int exp) {
+    struct Node* output = NULL;
+    struct Node* current = *headRef;
+    int count[10] = {0};
+
+    while (current != NULL) {
+        count[(current->data / exp) % 10]++;
+        current = current->next;
+    }
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    current = *headRef;
+    while (current != NULL) {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = current->data;
+        newNode->next = NULL;
+
+        int index = --count[(current->data / exp) % 10];
+        if (output == NULL) {
+            output = newNode;
+        } else {
+            struct Node* temp = output;
+            while (index-- > 0 && temp->next != NULL)
+                temp = temp->next;
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+        current = current->next;
+    }
+
+    *headRef = output;
+}
+
+void radixSortLinkedList(struct Node** headRef) {
+    int max = getMaxLinkedList(*headRef);
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSortLinkedList(headRef, exp);
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void printLinkedList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void append(struct Node** headRef, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *headRef;
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    if (*headRef == NULL) {
+        *headRef = newNode;
+        return;
+    }
+
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = newNode;
+}
+
+int main() {
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    printArray(arr, n);
+
+    radixSortArray(arr, n);
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    struct Node* head = NULL;
+    append(&head, 170);
+    append(&head, 45);
+    append(&head, 75);
+    append(&head, 90);
+    append(&head, 802);
+    append(&head, 24);
+    append(&head, 2);
+    append(&head, 66);
+
+    printf("Original linked list: ");
+    printLinkedList(head);
+
+    radixSortLinkedList(&head);
+    printf("Sorted linked list: ");
+    printLinkedList(head);
+
+    return 0;
+}
