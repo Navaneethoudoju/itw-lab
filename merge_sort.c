c72775df29fb31cfa28ff7276e2e62ba981162a7
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:09:22 +0530

    getmiddle function added

--- merge_sort.c
+++ merge_sort.c
@@ -19,3 +19,16 @@ struct ListNode* merge(struct ListNode* l1, struct ListNode* l2) {
     }
 }
 
+struct ListNode* getMiddle(struct ListNode* head) {
+    if (!head) return head;
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
