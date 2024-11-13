a4960cdbf78e02a3d47fa8c353f34a967e86eb9e
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:26:49 +0530

    push and printlist added

--- selectionsort.c
+++ selectionsort.c
@@ -12,3 +12,20 @@ void swap(struct Node* a, struct Node* b) {
     b->data = temp;
 }
 
+
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
