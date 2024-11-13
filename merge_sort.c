93c769b4d9efa059eee32d28428e0f16f3039ca7
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:10:21 +0530

    merge sort added

--- merge_sort.c
+++ merge_sort.c
@@ -32,3 +32,54 @@ struct ListNode* getMiddle(struct ListNode* head) {
 
     return slow;
 }
+
+struct ListNode* mergeSort(struct ListNode* head) {
+    if (!head || !head->next) return head;
+
+    struct ListNode* middle = getMiddle(head);
+    struct ListNode* nextToMiddle = middle->next;
+
+    middle->next = NULL;
+
+    struct ListNode* left = mergeSort(head);
+    struct ListNode* right = mergeSort(nextToMiddle);
+
+    return merge(left, right);
+}
+
+void printList(struct ListNode* head) {
+    while (head) {
+        printf("%d -> ", head->val);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+void push(struct ListNode** head_ref, int new_data) {
+    struct ListNode* new_node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    new_node->val = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    struct ListNode* head = NULL;
+
+
+    push(&head, 15);
+    push(&head, 10);
+    push(&head, 5);
+    push(&head, 20);
+    push(&head, 3);
+    push(&head, 2);
+
+    printf("Linked List before sorting:\n");
+    printList(head);
+
+    head = mergeSort(head);
+
+    printf("Linked List after sorting:\n");
+    printList(head);
+
+    return 0;
+}
