44da5ef8ef25e11d8095778141fe0ea6a92ba8b5
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:16:21 +0530

    Printlist and pushh functions added

--- bubble_sort.c
+++ bubble_sort.c
@@ -11,28 +11,7 @@ void swap(struct Node* a, struct Node* b) {
     b->data = temp;
 }
 
-void bubbleSortLinkedList(struct Node* head) {
-    int swapped;
-    struct Node* ptr1;
-    struct Node* lptr = NULL;
 
-    if (head == NULL)
-        return;
-
-    do {
-        swapped = 0;
-        ptr1 = head;
-
-        while (ptr1->next != lptr) {
-            if (ptr1->data > ptr1->next->data) {
-                swap(ptr1, ptr1->next);
-                swapped = 1;
-            }
-            ptr1 = ptr1->next;
-        }
-        lptr = ptr1;
-    } while (swapped);
-}
 
 void printList(struct Node* node) {
     while (node != NULL) {
@@ -49,24 +28,3 @@ void push(struct Node** head_ref, int new_data) {
     (*head_ref) = new_node;
 }
 
-int main() {
-    struct Node* a = NULL;
-
-    push(&a, 64);
-    push(&a, 34);
-    push(&a, 25);
-    push(&a, 12);
-    push(&a, 22);
-    push(&a, 11);
-    push(&a, 90);
-
-    printf("Unsorted Linked List: \n");
-    printList(a);
-
-    bubbleSortLinkedList(a);
-
-    printf("Sorted Linked List: \n");
-    printList(a);
-
-    return 0;
-}
