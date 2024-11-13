eea7768942b818c375d853ed23a6707f04ad907d
Navaneethoudoju <navaneeththoudoju888@gmail.com>
2024-11-13 11:02:52 +0530

    sorted insert added

---
+++ insertion_sort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void sortedInsert(struct Node** head_ref, struct Node* new_node) {
+    struct Node* current;
+    if (*head_ref == NULL || (*head_ref)->data >= new_node->data) {
+        new_node->next = *head_ref;
+        *head_ref = new_node;
+    } else {
+        current = *head_ref;
+        while (current->next != NULL && current->next->data < new_node->data) {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+void insertionSort(struct Node** head_ref) {
+    struct Node* sorted = NULL;
+    struct Node* current = *head_ref;
+    while (current != NULL) {
+        struct Node* next = current->next;
+        sortedInsert(&sorted, current);
+        current = next;
+    }
+    *head_ref = sorted;
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    push(&head, 5);
+    push(&head, 20);
+    push(&head, 4);
+    push(&head, 3);
+    push(&head, 30);
+
+    printf("Linked list before sorting:\n");
+    printList(head);
+
+    insertionSort(&head);
+
+    printf("Linked list after sorting:\n");
+    printList(head);
+
+    return 0;
+}
